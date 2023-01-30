0f1f2f6f6261b7b07877445f1020a2fb556b37db
rewqqx <rewqqx@gmail.com>
2023-01-30 19:51:51 +0300

    add psql

--- frontend/index.php
+++ frontend/index.php
@@ -1,4 +1,15 @@
 <?php
-echo 'Start';
+$dbHost = 'localhost';
+$dbPort = 49153;
+$user = 'postgres';
+$pass = 'postgrespw';
+$dbConn = pg_connect("host=$dbHost port=$dbPort dbname=postgres user=$user password=$pass")
+or die('Не удалось соединиться: ' . pg_last_error());
 
+$sql = "SELECT * FROM test.test_1";
+
+$pg_query = pg_query($dbConn, $sql);
+
+$arr = pg_fetch_all($pg_query);
+print_r($arr);
 ?>
