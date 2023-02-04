d149d29111eebf7adfa6dd07ecddc696a4b1ae0e
Bogdan Madzhuga <43291305+bogdanspbm@users.noreply.github.com>
2023-02-04 23:07:31 +0300

    Update userDatabaseAdapter.go

--- backend/src/utils/userDatabaseAdapter.go
+++ backend/src/utils/userDatabaseAdapter.go
@@ -5,16 +5,16 @@ type UserDatabase struct {
 }
 
 type User struct {
-	ID         int       `json:"id" db:"id"`
-	Name       string    `json:"user_name" db:"user_name"`
-	Surname    string    `json:"user_surname" db:"user_surname"`
-	Patronymic string    `json:"user_patronymic" db:"user_patronymic"`
-	Phone      string    `json:"phone" db:"phone"`
-	Birthdate  Timestamp `json:"birthdate" db:"birthdate"`
-	Password   string    `json:"password_hash" db:"password_hash"`
-	Mail       string    `json:"mail" db:"mail"`
-	RoleId     int       `json:"role_id" db:"role_id"`
-	Token      string    `json:"token" db:"token"`
+	ID         int        `json:"id" db:"id"`
+	Name       string     `json:"user_name" db:"user_name"`
+	Surname    string     `json:"user_surname" db:"user_surname"`
+	Patronymic string     `json:"user_patronymic" db:"user_patronymic"`
+	Phone      string     `json:"phone" db:"phone"`
+	Birthdate  *Timestamp `json:"birthdate" db:"birthdate"`
+	Password   string     `json:"password_hash" db:"password_hash"`
+	Mail       string     `json:"mail" db:"mail"`
+	RoleId     int        `json:"role_id" db:"role_id"`
+	Token      string     `json:"token" db:"token"`
 }
 
 type AuthToken struct {
@@ -22,14 +22,14 @@ type AuthToken struct {
 	Token string `json:"token"`
 }
 
-func (adapter *UserDatabase) getUser(id int) (user User, err error) {
-	user = User{}
-	err = adapter.database.Connection.Get(&user, "SELECT * FROM online_shop.users WHERE id=$1", id)
+func (adapter *UserDatabase) getUser(id int) (user *User, err error) {
+	user = &User{}
+	err = adapter.database.Connection.Get(user, "SELECT * FROM online_shop.users WHERE id=$1", id)
 
 	return
 }
 
-func (adapter *UserDatabase) createUser(user User) (id int64, err error) {
+func (adapter *UserDatabase) createUser(user *User) (id int64, err error) {
 	res, err := adapter.database.Connection.Exec("INSERT INTO online_shop.users (user_name, user_surname,user_patronymic, phone, birthdate, password_hash, mail, role_id) VALUES ($1, $2, $3, $4, $5, $6, $7, #8)", user.Name, user.Surname, user.Patronymic, user.Phone, user.Birthdate, user.Password, user.Mail, user.RoleId)
 	id, err = res.LastInsertId()
 	return
