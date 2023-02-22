bfcc8525ed928f34563acef760bed7fe16206b5f
Bogdan Madzhuga <43291305+bogdanspbm@users.noreply.github.com>
2023-02-22 20:58:17 +0800

    Auth Page

--- frontend/src/index.php
+++ frontend/src/index.php
@@ -1,5 +1,6 @@
 <link href='styles/DOM.css' rel='stylesheet' type='text/css'>
 <link href='styles/Default.css' rel='stylesheet' type='text/css'>
+<link href='styles/Auth.css' rel='stylesheet' type='text/css'>
 
 <?php
 
