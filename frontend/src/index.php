123892c401f3f6346e3d741bf6a89c73bf9071c5
Bogdan Madzhuga <43291305+bogdanspbm@users.noreply.github.com>
2023-02-22 18:30:11 +0800

    App Class

--- frontend/src/index.php
+++ frontend/src/index.php
@@ -3,33 +3,9 @@
 
 <?php
 
-include "./elements/DOM.inc";
-/*
-$dbHost = 'localhost';
-$dbPort = 49153;
-$user = 'postgres';
-$pass = 'postgrespw';
-$dbConn = pg_connect("host=$dbHost port=$dbPort dbname=postgres user=$user password=$pass")
-or die('Не удалось соединиться: ' . pg_last_error());
+include "./app/App.inc";
 
-$sql = "SELECT * FROM test.test_1";
-
-$pg_query = pg_query($dbConn, $sql);
-
-$arr = pg_fetch_all($pg_query);
-print_r($arr);*/
-
-$header = new DOM();
-$header->setStyle("header")->setText("Online Shop");
-
-$toolbar = new DOM();
-$toolbar->setStyle("toolbar");
-
-$container = new DOM();
-$container->setStyle("container")->append($toolbar);
-
-
-echo $header->getDOM();
-echo $container->getDOM();
+$app = new App();
+echo $app->getDOM();
 
 ?>
