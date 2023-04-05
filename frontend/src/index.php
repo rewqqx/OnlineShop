616dcea04758972812923235fe382e06612ffb7e
Bogdan Madzhuga <43291305+bogdanspbm@users.noreply.github.com>
2023-04-05 14:50:42 +0300

    Update

--- frontend/src/index.php
+++ frontend/src/index.php
@@ -5,11 +5,12 @@
 <?php
 include "./app/App.inc";
 include "./requests/Requests.inc";
+include "./objects/Auth.inc";
 
-echo Request::POSTRequest("http://127.0.0.1:8080/auth/", "{
-    \"mail\": \"admin@mail.ru\",
-    \"password\": \"admin\"}");
-echo Request::GETRequest("http://127.0.0.1:8080/items/");
+$auth = new \user\Auth("admin@mail.ru", "admin");
+echo $auth->toJson();
+echo Request::POSTRequest("http://127.0.0.1:8080/auth", $auth->toJson());
+//echo Request::GETRequest("http://127.0.0.1:8080/items/");
 
 
 $app = new App();
