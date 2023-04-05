c36118a728cbd22b3a4a4546126f571b22af1494
Bogdan Madzhuga <43291305+bogdanspbm@users.noreply.github.com>
2023-04-05 12:44:01 +0300

    update

--- frontend/src/index.php
+++ frontend/src/index.php
@@ -3,9 +3,13 @@
 <link href='styles/Auth.css' rel='stylesheet' type='text/css'>
 
 <?php
-
-include "./database/Database.inc";
 include "./app/App.inc";
+
+$url = "http://127.0.0.1:8080/items/";
+$q   = array("q"=>"PHP HTTP request");
+$page = file_get_contents($url . '?' . http_build_query($q));
+
+echo $page;
 
 $app = new App();
 echo $app->getDOM();
