21302025a76cc08114199ded6b0562be8c0251cc
Bogdan Madzhuga <43291305+bogdanspbm@users.noreply.github.com>
2023-05-10 16:21:26 +0300

    Update

--- frontend/src/index.php
+++ frontend/src/index.php
@@ -14,6 +14,7 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/frontend/src" . "/app/App.inc");
 require_once($_SERVER['DOCUMENT_ROOT'] . "/frontend/src" . "/requests/Requests.inc");
 require_once($_SERVER['DOCUMENT_ROOT'] . "/frontend/src" . "/objects/Auth.inc");
 require_once($_SERVER['DOCUMENT_ROOT'] . "/frontend/src" . "/widgets/ItemCard.inc");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/frontend/src" . "/requests/adapters/TagDatabaseAdapter.inc");
 require_once($_SERVER['DOCUMENT_ROOT'] . "/frontend/src" . "/requests/adapters/UserDatabaseAdapter.inc");
 require_once($_SERVER['DOCUMENT_ROOT'] . "/frontend/src" . "/requests/adapters/ItemDatabaseAdapter.inc");
 require_once($_SERVER['DOCUMENT_ROOT'] . "/frontend/src" . "/app/toolbar/Toolbar.inc");
