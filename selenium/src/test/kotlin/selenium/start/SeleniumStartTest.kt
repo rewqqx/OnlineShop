0d806be787b0867677548d2fb76d8e3f0e54ae72
Misha Diagilev <mishadyagilev@gmail.com>
2023-05-11 16:57:42 +0300

    add new tests

--- selenium/src/test/kotlin/selenium/start/SeleniumStartTest.kt
+++ selenium/src/test/kotlin/selenium/start/SeleniumStartTest.kt
@@ -13,11 +13,15 @@ import selenium.utils.*
 import java.io.File
 import java.nio.file.Paths
 import java.util.concurrent.TimeUnit
+import kotlin.test.DefaultAsserter.assertEquals
+import kotlin.test.DefaultAsserter.assertTrue
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 
 class SeleniumStartTest {
 
-    private val url: String = "http://localhost:63344/OnlineShop/frontend/src/index.php?_ijt=edgin57dokc0gs1qfq6ru08b24&_ij_reload=RELOAD_ON_SAVE"
+    private val url: String = "http://localhost:9050/frontend/src/"
 
 //    private val options: ChromeOptions = getDriverOptions()
 //    private val driver: WebDriver = ChromeDriver(options)
@@ -57,7 +61,164 @@ class SeleniumStartTest {
         }
     }
 
-//    @Test
+    @Test
+    fun checkPhonesFilter() {
+        driver.run {
+            get(url)
+            pageSource.shouldContain("Phones")
+            quit()
+        }
+    }
+
+    @Test
+    fun checkHouseFilter() {
+        driver.run {
+            get(url)
+            pageSource.shouldContain("House")
+            quit()
+        }
+    }
+
+    @Test
+    fun checkFruitsFilter() {
+        driver.run {
+            get(url)
+            pageSource.shouldContain("Fruits")
+            quit()
+        }
+    }
+
+    @Test
+    fun checkEducationFilter() {
+        driver.run {
+            get(url)
+            pageSource.shouldContain("Education")
+            quit()
+        }
+    }
+
+    @Test
+    fun checkFoodFilter() {
+        driver.run {
+            get(url)
+            pageSource.shouldContain("Food")
+            quit()
+        }
+    }
+
+    @Test
+    fun checkClothesFilter() {
+        driver.run {
+            get(url)
+            pageSource.shouldContain("Clothes")
+            quit()
+        }
+    }
+
+    @Test
+    fun checkBuildingsFilter() {
+        driver.run {
+            get(url)
+            pageSource.shouldContain("Buildings")
+            quit()
+        }
+    }
+
+    @Test
+    fun checkElectronicsFilter() {
+        driver.run {
+            get(url)
+            pageSource.shouldContain("Electronics")
+            quit()
+        }
+    }
+
+    @Test
+    fun checkCarsFilter() {
+        driver.run {
+            get(url)
+            pageSource.shouldContain("Cars")
+            quit()
+        }
+    }
+
+    @Test
+    fun testDeepCars() {
+        driver.run {
+            get(url)
+            val carsLink = findElement(By.linkText("Cars"))
+            carsLink.click()
+            assertEquals("http://localhost:9050/frontend/src/?tag_id=3", currentUrl)
+
+            val productElements = findElements(By.className("item-card"))
+            assertTrue(productElements.isNotEmpty())
+
+            for (productElement in productElements) {
+                val nameElement = productElement.findElement(By.className("item-desc"))
+                assertTrue(nameElement.isDisplayed())
+
+                val priceElement = productElement.findElement(By.className("item-image"))
+                assertTrue(priceElement.isDisplayed())
+            }
+
+            quit()
+        }
+    }
+
+    @Test
+    fun testDeepFruits() {
+        driver.run {
+            get(url)
+            val fruitLink = findElement(By.linkText("Fruits"))
+            fruitLink.click()
+            assertEquals("http://localhost:9050/frontend/src/?tag_id=8", currentUrl)
+
+            val productElements = findElements(By.className("item-card"))
+            assertTrue(productElements.isNotEmpty())
+
+            for (productElement in productElements) {
+                val nameElement = productElement.findElement(By.className("item-desc"))
+                assertTrue(nameElement.isDisplayed())
+
+                val priceElement = productElement.findElement(By.className("item-image"))
+                assertTrue(priceElement.isDisplayed())
+            }
+
+            val itemLink = findElement(By.linkText("Apple - 1 \$"))
+            itemLink.click()
+
+            pageSource.shouldContain("Apple")
+
+
+            quit()
+        }
+    }
+
+
+    @Test
+    fun openHTMLTest() {
+        driver.get("file:///" + path + "/src/test/resources/html/my_interactive_picture.html");
+        val check = driver.findElement(By.id("konva_interactive_container"))
+        check.shouldNotBe(null)
+        driver.quit()
+    }
+
+    //    @Test
+//    fun testLogInCheck() {
+//        driver.run {
+//            get(url)
+//            val loginLink = findElement(By.linkText("Sign In"))
+//            loginLink.click()
+//
+//            val pageSource = pageSource
+//            assertTrue(pageSource.contains("Password"), "Page should contain 'Password'")
+//
+//            quit()
+//        }
+//    }
+
+
+    //    @Test
 //    fun uploadJSONTest() {
 //        driver.get(url)
 //
@@ -84,14 +245,5 @@ class SeleniumStartTest {
 //
 //        driver.quit()
 //    }
-
-    @Test
-    fun openHTMLTest() {
-        driver.get("file:///" + path + "/src/test/resources/html/my_interactive_picture.html");
-        val check = driver.findElement(By.id("konva_interactive_container"))
-        check.shouldNotBe(null)
-        driver.quit()
-    }
-
 
 }
